5cd587d9d431a69dd9655a06b766d0e0c5933e03
Luciano Franzoi Filho <lucianofilhodombosco@gmail.com>
2024-09-03 01:09:22 -0300

    atualização função da SplashScreen

--- src/public/views/splash-screen.js
+++ src/public/views/splash-screen.js
@@ -2,25 +2,28 @@
     let logo = document.querySelector('.logo-header')
     let logoSpan = document.querySelectorAll('.logo')
     let logoImg = document.querySelector('.logo-img')
-   export function Splash(){
-     window.addEventListener('DOMContentLoaded',()=>{
-      setTimeout(()=>{
-        logoSpan.forEach((span,idx) => {
-          setTimeout(()=>{
-            span.classList.add('active')
-        }, (idx+1)*400);
+    export function Splash() {
+      return new Promise((resolve) => {
+          const intro = document.querySelector('.SplashScreen');
+          const logoSpan = document.querySelectorAll('.logo');
+          setTimeout(() => {
+              logoSpan.forEach((span, idx) => {
+                  setTimeout(() => {
+                      span.classList.add('active');
+                  }, (idx + 1) * 400); //tempo para cada Span ser ativado
+              });
+              setTimeout(() => {
+                  logoSpan.forEach((span, idx) => {
+                      setTimeout(() => {
+                          span.classList.remove('active');
+                          span.classList.add('fade');
+                      }, (idx + 1) * 50); // tempo entre cada Span
+                  });
+              }, 2000); //tempo antes de iniciar a animação de subir a tela
+              setTimeout(() => {
+                  intro.style.top = '-100vh';
+                  resolve();
+              }, 2300); //tempo até tela comecar a subir
+          });
       });
-
-      setTimeout(()=>{
-        logoSpan.forEach((span,idx)=>{
-          setTimeout(()=>{
-            span.classList.remove('active');
-            span.classList.add('fade');
-          }, (idx+1)*50)
-        })
-      },2000);
-      setTimeout(() => {
-        intro.style.top= '-100vh'
-      },2300);
-    })
-  })}
+  }
