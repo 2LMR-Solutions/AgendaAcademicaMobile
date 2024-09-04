6362e3ac112f1d2469c73279ba253f0dfa40b5e1
Leonardo <leonardoposs99@gmail.com>
2024-09-04 20:18:42 -0300

    Ajustes de caminhos de importação

--- src/public/app.js
+++ src/public/app.js
@@ -1,10 +1,13 @@
-import { showSplashScreen} from "./views/splash-screen.js";
-import { Usuario} from "./models/user_model.js";
+import { showSplashScreen } from "./views/splash-screen.js";
+import { Usuario } from "./models/user_model.js";
 import { Atividade } from "./models/atividade_model.js";
 import { UserAtividade } from "./models/user_atividade_model.js";
 import { cadastro_user } from "./controller/cadastro_user.js";
 
-document.addEventListener('DOMContentLoaded', showSplashScreen);
+if (document.body.id === 'index-page') {
+  document.addEventListener('DOMContentLoaded', showSplashScreen);
+}
+
 
 window.cadastro_user = cadastro_user;
 window.Usuario = Usuario;
