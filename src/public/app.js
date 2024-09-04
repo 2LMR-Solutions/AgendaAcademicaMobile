750ff23402d8f4ccfd52ff6eaa9693aada43d49f
Luciano Franzoi Filho <lucianofilhodombosco@gmail.com>
2024-09-04 10:17:43 -0300

    atualização do CSS para arrumar erros do ultimo commit

--- src/public/app.js
+++ src/public/app.js
@@ -1,14 +1,16 @@
- import { Splash } from "./views/splash-screen.js";
+import { Splash } from "./views/splash-screen.js";
  import { setProgress } from "./views/graficos.js";
  import { Usuario} from "./models/user_model.js"
  import { Atividade } from "./models/atividade_model.js";
  import { UserAtividade } from "./models/user_atividade_model.js";
  import { cadastro_user } from "./controller/cadastro_user.js";
 
-function updateCharts() {
-    setProgress('Grafico1', 75); //para mudar as porcentagens dos graficos tem que mandar o ID do grafico e a porcentagem 
-    setProgress('Grafico2', 50); //isso esta aqui para que quando terminar a animação da Splash Screen passar pelo Delay e 
-}                                //dai vai fazer as animaçoes do grafico
+ function updateCharts() {
+  setProgress('Grafico1', 75); //para mudar as porcentagens dos graficos tem que mandar o ID do grafico e a porcentagem 
+  setProgress('Grafico2', 50); //isso esta aqui para que quando terminar a animação da Splash Screen passar pelo Delay e 
+  setProgress('Grafico3', 43); //dai vai fazer as animaçoes do grafico
+  setProgress('Grafico4', 3);
+}    
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
