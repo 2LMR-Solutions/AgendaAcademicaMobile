0be3b6e551c905b5d2363ca84c029d8cd2560ee9
Luciano Franzoi Filho <lucianofilhodombosco@gmail.com>
2024-09-17 19:29:35 -0300

    refactor: função splash screen

--- src/public/app.js
+++ src/public/app.js
@@ -1,22 +1,16 @@
-// import { showSplashScreenIndex } from "./views/splash-screen-Index.js";
-// import { showSplashScreenTelaInicial } from "./views/splash-screen-Tela-Inicial.js";
-import { showSplashScreen } from "./views/splash-screen.js";
+import { SplashScreen } from "./views/splash-screen.js";
 import { Usuario } from "./models/user_model.js";
 import { Atividade } from "./models/atividade_model.js";
 import { UserAtividade } from "./models/user_atividade_model.js";
 import { cadastro_user } from "./controller/cadastro_user.js";
 import { agenda } from "./views/agenda.js";
-import { inactivityTime } from "./views/DetectorDeInatividade.js";
  
 
-if (document.body.id === 'index-page') {
-  document.addEventListener('DOMContentLoaded', showSplashScreen);
-}
-else if (document.body.id === 'agenda-page'){
+if (document.body.id === 'agenda-page'){
   agenda();
 }
 
-inactivityTime(); // tentar arrumar essa função 
+SplashScreen(); 
 
 window.cadastro_user = cadastro_user;
 window.Usuario = Usuario;
--- src/public/views/splash-screen.js
+++ src/public/views/splash-screen.js
@@ -1,79 +1,103 @@
-import { setProgress } from "./graficos.js";    
-    
-    let intro = document.querySelector('.SplashScreen')
-    let logo = document.querySelector('.logo-header')
-    let logoSpan = document.querySelectorAll('.logo')
-    let logoImg = document.querySelector('.logo-img')
-    function Splash() {
-      return new Promise((resolve) => {
-          const intro = document.querySelector('.SplashScreen');
-          const logoSpan = document.querySelectorAll('.logo');
-          setTimeout(() => {
-              logoSpan.forEach((span, idx) => {
-                  setTimeout(() => {
-                      span.classList.add('active');
-                  }, (idx + 1) * 400); //tempo para cada Span ser ativado
-              });
-              setTimeout(() => {
-                  logoSpan.forEach((span, idx) => {
-                      setTimeout(() => {
-                          span.classList.remove('active');
-                          span.classList.add('fade');
-                      }, (idx + 1) * 50); // tempo entre cada Span
-                  });
-              }, 2000); //tempo antes de iniciar a animação de subir a tela
-              setTimeout(() => {
-                  intro.style.top = '-100vh';
-                  resolve();
-              }, 2300); //tempo até tela comecar a subir
-          });
-      });
-  }
+import { setProgress } from "./graficos.js";
+
+let intro = document.querySelector('.SplashScreen')
+let logo = document.querySelector('.logo-header')
+let logoSpan = document.querySelectorAll('.logo')
+let logoImg = document.querySelector('.logo-img')
+function Splash() {
+    return new Promise((resolve) => {
+        const intro = document.querySelector('.SplashScreen');
+        const logoSpan = document.querySelectorAll('.logo');
+        setTimeout(() => {
+            logoSpan.forEach((span, idx) => {
+                setTimeout(() => {
+                    span.classList.add('active');
+                }, (idx + 1) * 400); //tempo para cada Span ser ativado
+            });
+            setTimeout(() => {
+                logoSpan.forEach((span, idx) => {
+                    setTimeout(() => {
+                        span.classList.remove('active');
+                        span.classList.add('fade');
+                    }, (idx + 1) * 50); // tempo entre cada Span
+                });
+            }, 2000); //tempo antes de iniciar a animação de subir a tela
+            setTimeout(() => {
+                intro.style.top = '-100vh';
+                resolve();
+            }, 2300); //tempo até tela comecar a subir
+        });
+    });
+}
 
 const SPLASH_SCREEN_KEY = 'showSplashScreen';
-const TIMEOUT = 10 * 60 * 1000; //tempo para mostrar a splashscreen 10 minutos,
-                                //a cada 10 minutos quando entrar no index, vai mostrar a splashscreen    
-                                
-//função para ver se a splash screen vai ser mostrada
- function shouldShowSplashScreen() {
+const TIMEOUT = 50000; // 10 minutos em milissegundos
+const now = new Date().getTime();
+
+function reiniciarTempo() {
+    localStorage.setItem(SPLASH_SCREEN_KEY, new Date().getTime());
+}
+
+function updateCharts() {
+    setProgress('Grafico1', 75);
+    setProgress('Grafico2', 50);
+    setProgress('Grafico3', 43);
+    setProgress('Grafico4', 3);
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function IrParaIndex() {
+    window.location.href = '/index.html';
+}
+
+function inactivityTimeSplash() {
     const lastVisit = localStorage.getItem(SPLASH_SCREEN_KEY);
     const now = new Date().getTime();
-    
     if (!lastVisit) {
         localStorage.setItem(SPLASH_SCREEN_KEY, now);
         return true;
     }
-    
+
     const timeSinceLastVisit = now - parseInt(lastVisit, 10);
-    
+
     if (timeSinceLastVisit > TIMEOUT) {
         localStorage.setItem(SPLASH_SCREEN_KEY, now);
+        if (document.body.id === 'index-page') {
+            Splash().then(() => {
+                delay(500).then(() => {
+                    updateCharts();
+                });
+            });
+        }else{
+            IrParaIndex();
+        }
         return true;
     }
-    
+
     return false;
 }
 
-export function showSplashScreen() {
-    if (shouldShowSplashScreen()) {
-        Splash().then(() => {
-            delay(500).then(() => {
-                updateCharts();
+export function SplashScreen() {
+    if (!inactivityTimeSplash()) {
+        if (document.body.id === 'index-page') {
+            Splash().then(() => {
+                delay(500).then(() => {
+                    updateCharts();
+                });
             });
-        });
-    } else {
-        document.querySelector('.SplashScreen').style.display = 'none';
-        updateCharts();
+        }
     }
 }
 
-function updateCharts() {
-  setProgress('Grafico1', 75); //para mudar as porcentagens dos graficos tem que mandar o ID do grafico e a porcentagem 
-  setProgress('Grafico2', 50); //isso esta aqui para que quando terminar a animação da Splash Screen passar pelo Delay e 
-  setProgress('Grafico3', 43); //dai vai fazer as animaçoes do grafico
-  setProgress('Grafico4', 3);
-}    
+window.onload = reiniciarTempo;
+document.onmousemove = reiniciarTempo;
+document.onkeydown = reiniciarTempo;
 
- function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+// Configura um intervalo para verificar a inatividade periodicamente
+setInterval(() => {
+    inactivityTimeSplash(); // Chama a função sem realizar outras ações
+}, 1000); // Verifica a cada 500 milissegundos
+
