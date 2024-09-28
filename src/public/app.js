22bac54906245370588cf97b7937ea1766ba6f66
Leonardo <leonardoposs99@gmail.com>
2024-09-28 02:53:11 -0300

    -refac app.js : mudança de if/else para switch cases

--- src/public/app.js
+++ src/public/app.js
@@ -1,34 +1,30 @@
 import { SplashScreen } from "./views/splash-screen.js";
-import { Usuario } from "./models/user_model.js";
-import { Atividade } from "./models/atividade_model.js";
-import { UserAtividade } from "./models/user_atividade_model.js";
-import { cadastro_user,login } from "./controller/cadastro_user.js";
+// import { Usuario } from "./models/user_model.js";
+// import { Atividade } from "./models/atividade_model.js";
+// import { UserAtividade } from "./models/user_atividade_model.js";
+// import { cadastro_user,login } from "./controller/cadastro_user.js";
 import { agenda } from "./views/agenda.js";
 import { preencherTarefasNoCalendario } from "./controller/preencherAgenda.js";
-import { cadastroT } from "./controller/incluir-atvidade.js";
+import { cadastroATV } from "./controller/incluir-atvidade.js";
 import { buscarAtividades } from "./controller/preencherTelaInicial.js";
 import { iniciarSubtarefas } from "./views/tela incluir tarefa/IncrementSubtarefas.js";
 import { mostrarNomeArquivo } from "./views/IncluirTarefa.js";
 
-
-if (document.body.id === 'agenda-page'){
+switch (document.body.id){
+case "agenda-page":
   agenda();
   preencherTarefasNoCalendario();
-}
-else if (document.body.id === 'incluirATV-page'){
+
+case "incluirATV-page":
   document.addEventListener('DOMContentLoaded', () => {
-    let test_btn = document.getElementById("test-btn");
-    if (test_btn) {
-      test_btn.onclick = cadastroT;
-    }
-    iniciarSubtarefas();
-    document.getElementById('arquivo').addEventListener('change', mostrarNomeArquivo);
-  });
+        let incluirATV_btn = document.getElementById("incluirATV_btn");
+        if (incluirATV_btn) {
+          incluirATV_btn.onclick = cadastroATV;
+        }
+        iniciarSubtarefas();
+        document.getElementById('arquivo').addEventListener('change', mostrarNomeArquivo);
+      });
 }
 
 SplashScreen(); 
 buscarAtividades();
-
-window.cadastro_user = cadastro_user;
-window.Usuario = Usuario;
-window.logar = login;
